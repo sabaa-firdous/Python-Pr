63c39d18e3097871bc212e2561243db28761e004
sabaa-firdous <sabaa.firdous@gmail.com>
2026-07-31 05:03:57 +0500

    Day 4 of Python Revision

---
+++ Day_4/bill.py
@@ -0,0 +1,6 @@
+import random 
+
+friends= ["Saba", "Ali", "Shoaib", "Ahtsham", "Wareesha", "Hafsa"]
+payment_by=random.choices(friends)
+
+print(payment_by)
---
+++ Day_4/randomization.py
@@ -0,0 +1,12 @@
+import random
+import my_module
+
+# random_integer = random.randint (0,100)
+# print(random_integer)
+# print(my_module.my_fav_number)
+
+stats_hnt=random.randint(0,1)
+if (stats_hnt==0):
+    print("Heads")
+else:
+    print("Tails")
