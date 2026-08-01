8846e364c409a6497bed4dc65a71a6d701d22bcf
sabaa-firdous <sabaa.firdous@gmail.com>
2026-08-01 15:37:28 +0500

    Day 5 Practice

--- Day_5/high_score.py
+++ Day_5/high_score.py
@@ -3,8 +3,14 @@ student_scores=[44, 33, 65, 61, 58, 47, 43, 99, 41, 84]
 # total_exam_score=sum(student_scores)
 # print(total_exam_score)
 
-sum=0
-for score in student_scores:
-    sum+=score
-print(sum)
+# sum=0
+# for score in student_scores:
+#     sum+=score
+# print(sum)
     
+
+max=0
+for score in student_scores:
+    if score>max:
+        max=score
+print(f"{max} is the highest score.")
---
+++ Day_5/range.py
@@ -0,0 +1,4 @@
+i=0
+for number in range(1,101):
+    i+=number
+print(i)
