680d77ca878caad79ce52cbb36606c1379d8003e
sabaa-firdous <sabaa.firdous@gmail.com>
2026-08-02 17:26:34 +0500

    Password Generator

--- Day_6/pswd_gen.py
+++ Day_6/pswd_gen.py
@@ -1,7 +1,6 @@
 #Password Generator
 
 import random
-
 letters=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']  
 symbols=['!', '#', '$', '%', '&','?', '@','(',')']
@@ -10,12 +9,36 @@ nr_letters=int(input('How many letters you want in your password?'))
 nr_numbers=int(input('How many numbers so you want in your Password?'))
 nr_symbols=int(input('How many special characters you want in your Password?'))
 
-pwd_letters=random.sample(letters, nr_letters)
-pwd_numbers=random.sample(numbers, nr_numbers)
-pwd_symbols=random.sample(symbols,nr_symbols)
+#Silly way 
 
-password_list = pwd_letters + pwd_numbers + pwd_symbols
-#random.shuffle(password_list)
-password = "" .join(password_list)
+# pwd_letters=random.sample(letters, nr_letters)
+# pwd_numbers=random.sample(numbers, nr_numbers)
+# pwd_symbols=random.sample(symbols,nr_symbols)
 
-print(f'Your Password is  {password}')
+# password_list = pwd_letters + pwd_numbers + pwd_symbols
+# #random.shuffle(password_list)
+# password = "" .join(password_list)
+
+# print(f'Your Password is  {password}')
+
+
+#Smart and Hard way 
+
+passwordl=[]
+for char in range(0, nr_letters):
+    passwordl.append(random.choice(letters))
+
+for char in range(0, nr_numbers):
+    passwordl.append(random.choice(numbers))
+
+for char in range(0, nr_symbols):
+    passwordl.append(random.choice(symbols))
+
+print(passwordl)
+random.shuffle(passwordl)
+
+password=""
+for char in passwordl:
+    password+=char
+
+print(f' Your Password is: {password}')
