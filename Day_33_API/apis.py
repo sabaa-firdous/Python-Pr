8c8d3bdd8fd760af7d9a7adfc96ca346432d23a7
sabaa-firdous <sabaa.firdous@gmail.com>
2026-08-05 23:46:24 +0500

    API

--- Day_33_API/apis.py
+++ Day_33_API/apis.py
@@ -1,11 +1,12 @@
 import requests
 
 response= requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
-print(response.status_code)
-if response.status_code != 200:
-    raise Exception("Bad response from ISS API")
-elif response.status_code==401:
-    raise Exception("You are not allowed to this data")
-elif response.status_code==401:
-    raise Exception("This resource does not exist")
+response.raise_for_status()
+# print(response)
+# print(response.status_code)
+# if response.status_code != 200:
+#     raise Exception("Bad response from ISS API")
+# elif response.status_code==401:
+#     raise Exception("You are not allowed to this data")
+# elif response.status_code==401:
+#     raise Exception("This resource does not exist")
