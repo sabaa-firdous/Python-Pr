342bb2d7e83d568f3eb0c0aca7d7f1402d81ee95
sabaa-firdous <sabaa.firdous@gmail.com>
2026-08-05 23:50:00 +0500

    APIs

--- Day_33_API/apis.py
+++ Day_33_API/apis.py
@@ -2,6 +2,8 @@ import requests
 
 response= requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
+data=response.json()['iss_position']
+print(data)
 # print(response)
 # print(response.status_code)
 # if response.status_code != 200:
